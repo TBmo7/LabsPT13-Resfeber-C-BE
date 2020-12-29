c1dd81c7c7aaba292984f97648c647db7584f78c
Troy Moore <tbmoore85@gmail.com>
2020-12-29 11:05:01 -0500

    Feature added: added temp_ds_api/viz endpoint

--- api/temp_ds_api/temp_ds_router.js
+++ api/temp_ds_api/temp_ds_router.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 const db = require('../../data/db-config.js')
 
-router.get('/Fuel/:id',(req,res)=>{
+router.get('/fuel/:id',(req,res)=>{
     const state = req.params;
     console.log(state)
     const requestOptions = {
@@ -15,6 +15,23 @@ router.get('/Fuel/:id',(req,res)=>{
     .get(`http://labspt13-resfeber-c-ds.eba-ai47pmnm.us-east-1.elasticbeanstalk.com/fuel/${state.id}`)
     .then(response=>{
         console.log(response)
+        data = response.data
+    })
+    .then(thing=>{
+        res.status(200).json(data)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({message:'Error fetching API', error:err})
+    })
+})
+
+router.get('/viz/:id', (req,res)=>{
+    const state = req.params;
+    
+    axios
+    .get(`http://labspt13-resfeber-c-ds.eba-ai47pmnm.us-east-1.elasticbeanstalk.com/viz/${state.id}`)
+    .then(response=>{
         data = response.data
     })
     .then(thing=>{
